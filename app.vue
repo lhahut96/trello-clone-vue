36b64e3c30218c06a763791633a8719aa76daa3e
Lucas <lhahut96@gmail.com>
2024-03-24 09:13:06 -0700

    Fix logo and fontsize on index page

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="p-10 h-[100vh] bg-teal-600 overflow-auto flex-col">
     <div class="flex items-center mb-10">
-      <img src="/logo.png" alt="" class="mr-3" width="200" />
-      <h1 class="text-5xl font-extrabold">Lucas clone Trello</h1>
+      <img src="/logo.png" alt="" class="mr-3" width="80" />
+      <h1 class="text-4xl font-extrabold">Lucas clone Trello</h1>
     </div>
     <TrelloBoard />
   </div>
